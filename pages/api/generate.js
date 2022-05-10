3cb4050d0973469606cf64f4ee7cf6ad2c2a41db
minfanzhang <zhang.minfan@mail.utoronto.ca>
2022-05-10 17:49:39 -0400

    New working version

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -4,9 +4,9 @@ const openai = new OpenAIApi(new Configuration({apiKey: process.env.OPENAI_SECRE
 
 export default async function (req, res) {
   const completion = await openai.createCompletion("text-davinci-002", {
-    prompt: generatePrompt(req.body.animal),
+    prompt: req.body.prompttext,
     temperature: 0.5,
-    max_tokens: 4,
+    max_tokens: 8,
     top_p: 1.0,
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
@@ -14,6 +14,6 @@ export default async function (req, res) {
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
+/*function generatePrompt(animal) {
   return `write a poem about dinousaurs in the snow`;
-}
+}*/
